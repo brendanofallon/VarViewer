1496fc567ed861416bf187021571687c861eb7ff
Brendan <brendan@brendan.workstation>
2013-03-27 07:44:42 -0600

    Additional commenting

--- src/varviewer/server/variant/PreAnnotatedVarReqHandler.java
+++ src/varviewer/server/variant/PreAnnotatedVarReqHandler.java
@@ -1,6 +1,5 @@
 package varviewer.server.variant;
 
-import java.util.Date;
 import java.util.List;
 
 import varviewer.server.CachingSampleSource;
@@ -31,27 +30,27 @@ public class PreAnnotatedVarReqHandler implements VariantRequestHandler {
 
 	@Override
 	public VariantRequestResult queryVariant(VariantRequest req) {
-		Date begin = new Date();
+		//Date begin = new Date();
 		
 		
-		Date init = new Date();
-		System.out.println("Time to initialize: " + (init.getTime()-begin.getTime())/1000.0);
+		//Date init = new Date();
+		//System.out.println("Time to initialize: " + (init.getTime()-begin.getTime())/1000.0);
 		
 		//First: Obtain the "raw" list of variants, unfiltered and (mostly) unannotated
 		VariantCollection vars = variantSource.getVariantsForSample(req.getSampleIDs().get(0));
 
-		Date readVars = new Date();
-		System.out.println("Time to read: " + (readVars.getTime()-init.getTime())/1000.0);
+		//Date readVars = new Date();
+		//System.out.println("Time to read: " + (readVars.getTime()-init.getTime())/1000.0);
 	
-		Date annotate = new Date();
-		System.out.println("Time to annotate: " + (annotate.getTime()-readVars.getTime())/1000.0);
+		//Date annotate = new Date();
+		//System.out.println("Time to annotate: " + (annotate.getTime()-readVars.getTime())/1000.0);
 		
 		//Third : Apply filters to the variants and return only those passing all filters
 		FilterExecutor filterExec = new SimpleFilterExecutor();
 		List<Variant> passingVars = filterExec.filterAll(vars, req.getFilters());
 		
-		Date filter = new Date();
-		System.out.println("Time to filter: " + (filter.getTime()-annotate.getTime())/1000.0);
+		//Date filter = new Date();
+		//System.out.println("Time to filter: " + (filter.getTime()-annotate.getTime())/1000.0);
 		
 		VariantRequestResult result = new VariantRequestResult();
 		result.setSampleID(req.getSampleIDs().get(0));
