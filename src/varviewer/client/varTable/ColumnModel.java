445d5330fdc7612ffb097b7c9080151a8b4193e4
cmkrueger17 <christopher.krueger@aruplab.com>
2017-02-22 09:57:07 -0700

    Revert "Exon func filter update. Added variant.type column too."
    This reverts commit f55499e6efbde999c6494fc613126a0496f10b36.

--- src/varviewer/client/varTable/ColumnModel.java
+++ src/varviewer/client/varTable/ColumnModel.java
@@ -1,173 +1,214 @@
 package varviewer.client.varTable;
 
-import com.google.gwt.core.shared.GWT;
-import com.google.gwt.resources.client.ImageResource;
-import com.google.gwt.safehtml.shared.SafeHtml;
-import com.google.gwt.user.cellview.client.Column;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
 import varviewer.shared.variant.Variant;
 
-public class ColumnModel
-{
-  List<String> keys = new ArrayList();
-  Map<String, VarAnnotation> colMap = new HashMap();
-  List<ColumnModelListener> listeners = new ArrayList();
-  
-  public ColumnModel()
-  {
-    addColumn(ColumnStore.getStore().getColumnForID("gene"));
-    addColumn(ColumnStore.getStore().getColumnForID("exon.function"));
-    addColumn(ColumnStore.getStore().getColumnForID("variant.type"));
-    addColumn(ColumnStore.getStore().getColumnForID("zygosity"));
-    addColumn(ColumnStore.getStore().getColumnForID("cdot"));
-    addColumn(ColumnStore.getStore().getColumnForID("pdot"));
-    addColumn(ColumnStore.getStore().getColumnForID("pop.freq.maf"));
-    addColumn(ColumnStore.getStore().getColumnForID("disease.pics"));
-    addColumn(ColumnStore.getStore().getColumnForID("rsnum"));
-    addColumn(ColumnStore.getStore().getColumnForID("igv.link"));
-  }
-  
-  public void setColumns(List<VarAnnotation<?>> annos)
-  {
-    this.colMap.clear();
-    this.keys.clear();
-    for (VarAnnotation<?> anno : annos) {
-      addColumn(anno, true);
-    }
-    fireColumnChange();
-  }
-  
-  public void addColumn(VarAnnotation<?> varAnno, boolean silent)
-  {
-    this.keys.add(varAnno.id);
-    this.colMap.put(varAnno.id, varAnno);
-    if (!silent) {
-      fireColumnChange();
-    }
-  }
-  
-  public void addColumn(VarAnnotation<?> varAnno)
-  {
-    addColumn(varAnno, false);
-  }
-  
-  public void removeColumn(String id)
-  {
-    this.keys.remove(id);
-    this.colMap.remove(id);
-    fireColumnChange();
-  }
-  
-  public void removeColumnsByClass(Class<?> clz)
-  {
-    List<String> keysToRemove = new ArrayList();
-    for (String key : this.colMap.keySet())
-    {
-      VarAnnotation<?> varAnno = (VarAnnotation)this.colMap.get(key);
-      if (varAnno.getClass().equals(clz)) {
-        keysToRemove.add(key);
-      }
-    }
-    for (String key : keysToRemove)
-    {
-      this.colMap.remove(key);
-      this.keys.remove(key);
-    }
-    if (keysToRemove.size() > 0) {
-      fireColumnChange();
-    }
-  }
-  
-  public List<String> getKeys()
-  {
-    return this.keys;
-  }
-  
-  public String writeHeader()
-  {
-    StringBuilder str = new StringBuilder();
-    for (String key : getKeys())
-    {
-      String txt = getUserTextForKey(key);
-      str.append(txt + ",");
-    }
-    return str.toString();
-  }
-  
-  public String writeVariant(Variant var)
-  {
-    StringBuilder str = new StringBuilder();
-    for (String key : getKeys())
-    {
-      Object val = getColumnForKey(key).getValue(var);
-      String valStr = "?";
-      if (val != null)
-      {
-        valStr = val.toString();
-        if ((val instanceof SafeHtml))
-        {
-          SafeHtml html = (SafeHtml)val;
-          valStr = html.asString().replaceAll("\\<.*?>", "");
-        }
-        if ((val instanceof ImageResource))
-        {
-          ImageResource imgRes = (ImageResource)val;
-          valStr = "(image)";
-          if (imgRes.equals(this.resources.refImage())) {
-            valStr = "ref";
-          }
-          if (imgRes.equals(this.resources.hetImage())) {
-            valStr = "het";
-          }
-          if (imgRes.equals(this.resources.homImage())) {
-            valStr = "hom";
-          }
-        }
-      }
-      str.append(valStr + ",");
-    }
-    return str.toString();
-  }
-  
-  public Column<Variant, ?> getColumnForKey(String key)
-  {
-    return ((VarAnnotation)this.colMap.get(key)).col;
-  }
-  
-  public boolean containsColumn(String key)
-  {
-    return this.colMap.containsKey(key);
-  }
-  
-  public String getUserTextForKey(String key)
-  {
-    return ((VarAnnotation)this.colMap.get(key)).userText;
-  }
-  
-  public VarAnnotation<?> getVarAnnoForKey(String key)
-  {
-    return (VarAnnotation)this.colMap.get(key);
-  }
-  
-  public void addListener(ColumnModelListener l)
-  {
-    this.listeners.add(l);
-  }
-  
-  public void removeListener(ColumnModelListener l)
-  {
-    this.listeners.remove(l);
-  }
-  
-  private void fireColumnChange()
-  {
-    for (ColumnModelListener l : this.listeners) {
-      l.columnStateChanged(this);
-    }
-  }
-  
-  VarPageResources resources = (VarPageResources)GWT.create(VarPageResources.class);
+import com.google.gwt.core.shared.GWT;
+import com.google.gwt.resources.client.ImageResource;
+import com.google.gwt.safehtml.shared.SafeHtml;
+import com.google.gwt.user.cellview.client.Column;
+
+/**
+ * The list of columns / annotations that should be displayed in the VarTable. Each column
+ * is associated with a VarAnnotation that contains a user-readable label and the actual
+ * TextColumn used to render the data.  
+ * @author brendan
+ *
+ */
+public class ColumnModel {
+	
+	//Ordered list of annotation keys and their respective columns
+	List<String> keys = new ArrayList<String>();
+	Map<String, VarAnnotation> colMap = new HashMap<String, VarAnnotation>();
+	List<ColumnModelListener> listeners = new ArrayList<ColumnModelListener>();
+	
+	public ColumnModel() {
+//		addColumn( ColumnStore.getStore().getColumnForID("contig"));
+//		addColumn( ColumnStore.getStore().getColumnForID("pos"));
+//		addColumn( ColumnStore.getStore().getColumnForID("some.annotation"));
+//		addColumn( ColumnStore.getStore().getColumnForID("another.annotation"));
+//		addColumn( ColumnStore.getStore().getColumnForID("quality"));
+		addColumn( ColumnStore.getStore().getColumnForID("gene"));
+		addColumn( ColumnStore.getStore().getColumnForID("exon.function"));
+		addColumn( ColumnStore.getStore().getColumnForID("zygosity"));
+		addColumn( ColumnStore.getStore().getColumnForID("cdot"));
+		addColumn( ColumnStore.getStore().getColumnForID("pdot"));
+		addColumn( ColumnStore.getStore().getColumnForID("pop.freq.maf"));
+		addColumn( ColumnStore.getStore().getColumnForID("disease.pics"));
+		addColumn( ColumnStore.getStore().getColumnForID("rsnum"));
+		addColumn( ColumnStore.getStore().getColumnForID("igv.link"));
+	}
+	
+	public void setColumns(List<VarAnnotation<?>> annos) {
+		colMap.clear();
+		keys.clear();
+		for(VarAnnotation<?> anno : annos) {
+			addColumn(anno, true);
+		}
+		fireColumnChange();
+	}
+	
+	/**
+	 * Add the new column, if silent, don't fire a column change event
+	 * @param varAnno
+	 * @param silent
+	 */
+	public void addColumn(VarAnnotation<?> varAnno, boolean silent) {
+		keys.add(varAnno.id);
+		colMap.put(varAnno.id, varAnno);
+		if (! silent)
+			fireColumnChange();
+	}
+	
+	public void addColumn(VarAnnotation<?> varAnno) {
+		addColumn(varAnno, false);
+	}
+	
+	public void removeColumn(String id) {
+		keys.remove(id);
+		colMap.remove(id);
+		fireColumnChange();
+	}
+	
+	public void removeColumnsByClass(Class<?> clz) {
+		List<String> keysToRemove = new ArrayList<String>();
+		for(String key : colMap.keySet()) {
+			VarAnnotation<?> varAnno = colMap.get(key);
+			if (varAnno.getClass().equals(clz)) {
+				keysToRemove.add(key);
+			}
+		}
+		
+		for(String key: keysToRemove) {
+			colMap.remove(key);
+			keys.remove(key);
+		}
+		
+		if (keysToRemove.size()>0)
+			fireColumnChange();
+	}
+	
+	/**
+	 * Returns the list used to store all current annotation keys
+	 * @return
+	 */
+	public List<String> getKeys() {
+		return keys;
+	}
+	
+	/**
+	 * Obtain a String with the user text for each column separated by commas, suitable as a header
+	 * for the variants written by writeVarian(var)  
+	 * @return
+	 */
+	public String writeHeader() {
+		StringBuilder str = new StringBuilder();
+		for(String key : getKeys()) {
+			String txt = this.getUserTextForKey(key);
+			str.append(txt + ",");
+		}
+		
+		return str.toString();
+	}
+	
+	/**
+	 * Creates a string representing the given Variant using the currently active columns
+	 * in this model
+	 * @param var
+	 * @return
+	 */
+	public String writeVariant(Variant var) {
+		StringBuilder str = new StringBuilder();
+		for(String key : getKeys()) {
+			Object val = getColumnForKey(key).getValue(var);
+			String valStr = "?";
+			if (val != null) {
+				valStr = val.toString();
+				
+				if (val instanceof SafeHtml) {
+					SafeHtml html = (SafeHtml) val;
+					valStr = html.asString(). replaceAll("\\<.*?>","");
+				}
+				
+				if (val instanceof ImageResource) {
+					ImageResource imgRes = (ImageResource)val;
+					valStr = "(image)";
+					if (imgRes.equals(resources.refImage())) {
+						valStr = "ref";
+					}
+					if (imgRes.equals(resources.hetImage())) {
+						valStr = "het";
+					}
+					if (imgRes.equals(resources.homImage())) {
+						valStr = "hom";
+					}
+				}
+			}
+			
+			str.append( valStr + ",");
+		}
+		return str.toString();
+	}
+	
+	/**
+	 * Get the column used to render the given annotation key, or null if there's
+	 * no such column
+	 * @param key
+	 * @return
+	 */
+	public Column<Variant, ?> getColumnForKey(String key) {
+		return colMap.get(key).col;
+	}
+
+	/**
+	 * True if this column model contains a column with the given id
+	 * @param key
+	 * @return
+	 */
+	public boolean containsColumn(String key) {
+		return colMap.containsKey(key);
+	}
+	
+	/**
+	 * A user-friendly String describing this annotation
+	 * @param key
+	 * @return
+	 */
+	public String getUserTextForKey(String key) {
+		return colMap.get(key).userText;
+	}
+	
+	/**
+	 * Return the VarAnno object associated with the key
+	 * @param key
+	 * @return
+	 */
+	public VarAnnotation<?> getVarAnnoForKey(String key) {
+		return colMap.get(key);
+	}
+
+	/**
+	 * Add a listener that will be notified when this model changes
+	 * @param l
+	 */
+	public void addListener(ColumnModelListener l) {
+		listeners.add(l);
+	}
+	
+	public void removeListener(ColumnModelListener l) {
+		listeners.remove(l);
+	}
+	
+	private void fireColumnChange() {
+		for(ColumnModelListener l : listeners) {
+			l.columnStateChanged(this);
+		}
+	}
+	
+	VarPageResources resources = (VarPageResources) GWT.create(VarPageResources.class);
+
 }
--- src/varviewer/client/varTable/filters/ExonFuncFilterConfig.java
+++ src/varviewer/client/varTable/filters/ExonFuncFilterConfig.java
@@ -1,145 +1,142 @@
 package varviewer.client.varTable.filters;
 
-import com.google.gwt.user.client.Element;
-import com.google.gwt.user.client.ui.CheckBox;
-import com.google.gwt.user.client.ui.DockLayoutPanel;
-import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.VerticalPanel;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
 import varviewer.shared.varFilters.ExonFuncFilter;
 import varviewer.shared.variant.VariantFilter;
 
-public class ExonFuncFilterConfig
-  extends FilterConfig
-{
-  private Map<String, CheckBox> exonTypes = new HashMap();
-  private ExonFuncFilter filter;
-  
-  public ExonFuncFilterConfig(FilterBox parent)
-  {
-    super(parent);
-    
-    VariantFilter fil = parent.getFilter();
-    if ((fil instanceof ExonFuncFilter)) {
-      this.filter = ((ExonFuncFilter)fil);
-    } else {
-      throw new IllegalArgumentException("Incorrect filter type in ExonFuncFilter");
-    }
-    VerticalPanel panel = new VerticalPanel();
-    Label lab = new Label("Exclude variants by type:");
-    panel.add(lab);
-    panel.add(addType("Noncoding RNA", true));
-    panel.add(addType("Non-frameshifting indels", false));
-    panel.add(addType("Frameshifting indels", false));
-    panel.add(addType("Stop gains & losses", false));
-    panel.add(addType("Synonymous", true));
-    panel.add(addType("Missense", false));
-    
-    Label lab2 = new Label("Exclude variants by region:");
-    panel.add(lab2);
-    panel.add(addType("Exonic", false));
-    panel.add(addType("Exonic;splicing", false));
-    panel.add(addType("Splicing (no additional annotation)", false));
-    panel.add(addType("Intronic (and not splicing)", true));
-    panel.add(addType("Intergenic", true));
-    panel.add(addType("UTR", true));
-    
-    panel.getElement().addClassName("exonFuncTable");
-    for (int i = 0; i < panel.getWidgetCount(); i++) {
-      panel.setCellHeight(panel.getWidget(i), "20px");
-    }
-    updateInteriorLabelText();
-    this.interiorPanel.add(panel);
-    panel.setHeight("350px");
-    this.interiorPanel.setWidth("300px");
-    this.interiorPanel.setHeight("400px");
-  }
-  
-  private CheckBox addType(String userText, boolean checked)
-  {
-    CheckBox box = new CheckBox(userText);
-    box.setValue(Boolean.valueOf(checked));
-    this.exonTypes.put(userText, box);
-    return box;
-  }
-  
-  public void updateInteriorLabelText()
-  {
-    int count = 0;
-    List<String> types = new ArrayList();
-    for (String key : this.exonTypes.keySet())
-    {
-      CheckBox box = (CheckBox)this.exonTypes.get(key);
-      if (box.getValue().booleanValue())
-      {
-        count++;
-        types.add(key);
-      }
-    }
-    if (count == 0)
-    {
-      this.parentBox.setInteriorText("Excluding NO variant types");
-      return;
-    }
-    if (count > 4)
-    {
-      this.parentBox.setInteriorText("Excluding " + count + " variant types");
-      return;
-    }
-    String str = "";
-    for (int i = 0; i < types.size() - 1; i++) {
-      str = str + (String)types.get(i) + ", ";
-    }
-    str = str + " and " + (String)types.get(types.size() - 1);
-    this.parentBox.setInteriorText("Excluding " + str + " variants");
-  }
-  
-  protected boolean validateAndUpdateFilter()
-  {
-    for (String key : this.exonTypes.keySet())
-    {
-      CheckBox box = (CheckBox)this.exonTypes.get(key);
-      if (key.equals("Intergenic")) {
-        this.filter.setExcludeIntergenic(box.getValue().booleanValue());
-      }
-      if (key.equals("Intronic (and not splicing)")) {
-        this.filter.setExcludeIntronic(box.getValue().booleanValue());
-      }
-      if (key.equals("UTR")) {
-        this.filter.setExcludeUTR(box.getValue().booleanValue());
-      }
-      if (key.equals("Synonymous")) {
-        this.filter.setExcludeSynonymous(box.getValue().booleanValue());
-      }
-      if (key.equals("Non-frameshifting indels")) {
-        this.filter.setExcludeNonFrameshift(box.getValue().booleanValue());
-      }
-      if (key.equals("Frameshifting indels")) {
-        this.filter.setExcludeFrameshift(box.getValue().booleanValue());
-      }
-      if (key.equals("Splicing (no additional annotation)")) {
-        this.filter.setExcludeSplicing(box.getValue().booleanValue());
-      }
-      if (key.equals("Exonic;splicing")) {
-        this.filter.setExcludeExonicSplicing(box.getValue().booleanValue());
-      }
-      if (key.equals("Exonic")) {
-        this.filter.setExcludeExonic(box.getValue().booleanValue());
-      }
-      if (key.equals("Missense")) {
-        this.filter.setExcludeNonsynonymous(box.getValue().booleanValue());
-      }
-      if (key.equals("Noncoding RNA")) {
-        this.filter.setExcludeNCRNA(box.getValue().booleanValue());
-      }
-      if (key.equals("Stop gains & losses")) {
-        this.filter.setExcludeStopGainsLosses(box.getValue().booleanValue());
-      }
-    }
-    updateInteriorLabelText();
-    return true;
-  }
+import com.google.gwt.user.client.ui.CheckBox;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+/**
+ * VariantFilter config popup box allowing users to select what types of variants to include
+ * and or exclude
+ * @author brendan
+ *
+ */
+public class ExonFuncFilterConfig extends FilterConfig {
+
+	private Map<String, CheckBox> exonTypes = new HashMap<String, CheckBox>();
+	private ExonFuncFilter filter;
+	
+	public ExonFuncFilterConfig(FilterBox parent) {
+		super(parent);
+		
+		VariantFilter fil = parent.getFilter();
+		if (fil instanceof ExonFuncFilter) {
+			this.filter = (ExonFuncFilter)fil;
+		}
+		else {
+			throw new IllegalArgumentException("Incorrect filter type in ExonFuncFilter");
+		}
+		
+		VerticalPanel panel = new VerticalPanel();
+		Label lab = new Label("Exclude variants by type:");
+		panel.add(lab);
+		
+		
+		panel.add( addType("Intergenic", true) );
+		panel.add( addType("Intronic (and not splicing)", true) );
+		panel.add( addType("UTR", true) );
+		panel.add( addType("Synonymous", true) );
+		panel.add( addType("Noncoding RNA", true) );
+		panel.add( addType("Non-frameshifting indels", false) );
+		panel.add( addType("Frameshifting indels",  false) );
+		panel.add( addType("Stop gains & losses",  false) );
+		panel.add( addType("Splicing", false) );
+		panel.add( addType("Missense", false) );
+		
+		panel.getElement().addClassName("exonFuncTable");
+		for(int i=0; i<panel.getWidgetCount(); i++) {
+			panel.setCellHeight(panel.getWidget(i), "20px");	
+		}
+		
+		updateInteriorLabelText(); //Set interior text in parental filterbox
+		interiorPanel.add(panel);
+		panel.setHeight("350px");
+		interiorPanel.setWidth("300px");
+		interiorPanel.setHeight("400px");
+		
+	}
+
+	private CheckBox addType(String userText, boolean checked) {
+		CheckBox box = new CheckBox(userText);
+		box.setValue(checked);
+		exonTypes.put(userText, box);
+		return box;
+	}
+	
+	public void updateInteriorLabelText() {
+		int count = 0;
+		List<String> types = new ArrayList<String>();
+		for(String key : exonTypes.keySet()) {
+			CheckBox box = exonTypes.get(key);
+			if (box.getValue()) {
+				count++;
+				types.add(key);
+			}
+		}
+		
+		if (count == 0) {
+			parentBox.setInteriorText("Excluding NO variant types");
+			return;
+		}
+		if (count > 4) {
+			parentBox.setInteriorText("Excluding " + count + " variant types");
+			return;
+		}
+		
+		String str = "";
+		for(int i=0; i<types.size()-1; i++) {
+			str = str + types.get(i) + ", ";
+		}
+		str = str + " and " + types.get( types.size()-1);
+		parentBox.setInteriorText("Excluding " + str + " variants");
+	}
+	
+	@Override
+	protected boolean validateAndUpdateFilter() {
+		for(String key : exonTypes.keySet()) {
+			CheckBox box = exonTypes.get(key);
+			
+			if (key.equals("Intergenic"))
+				this.filter.setExcludeIntergenic( box.getValue() ); 
+				
+			if (key.equals("Intronic (and not splicing)")) 
+				this.filter.setExcludeIntronic( box.getValue() ); 
+				
+			if (key.equals("UTR"))
+				this.filter.setExcludeUTR( box.getValue() ); 
+				
+			if (key.equals( "Synonymous")) 
+				this.filter.setExcludeSynonymous( box.getValue() ); 
+				
+			if (key.equals( "Non-frameshifting indels")) 
+				this.filter.setExcludeNonFrameshift( box.getValue() ); 
+				
+			if (key.equals( "Frameshifting indels")) 
+				this.filter.setExcludeFrameshift( box.getValue() ); 
+			
+			if (key.equals( "Splicing")) 
+				this.filter.setExcludeSplicing( box.getValue() ); 
+				
+			if (key.equals( "Missense")) 
+				this.filter.setExcludeNonsynonymous( box.getValue() ); 
+				
+			if (key.equals( "Noncoding RNA"))
+				this.filter.setExcludeNCRNA( box.getValue() );
+			
+			if (key.equals( "Stop gains & losses")) 
+				this.filter.setExcludeStopGainsLosses( box.getValue() ); 
+				
+		}
+		
+		updateInteriorLabelText();
+		return true;
+	}
+
 }
